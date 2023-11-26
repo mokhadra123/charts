e37b70878c42119bb6a9a263b97bd9533d252025
unknown <mokhadra008@gmail.com>
2023-11-26 18:48:36 +0200

    first commit in charts task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartsModule } from 'src/charts/charts.module';
+import { StatisticComponent } from 'src/charts/statistic/statistic.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ChartsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
