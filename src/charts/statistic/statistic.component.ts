64edf000b65ec6e40c66220bb61caf040d5f24bf
unknown <mokhadra008@gmail.com>
2023-11-28 14:14:36 +0200

    responsive

--- src/charts/statistic/statistic.component.ts
+++ src/charts/statistic/statistic.component.ts
@@ -44,15 +44,10 @@ export class StatisticComponent implements OnInit {
 
   constructor() {
     const months = [' يناير', 'فبراير ', 'مارس ', 'ابريل'];
-
     const accidents = [
-      { type: 'كبيرة', data: [0, 0, 0, 0] },
+      { type: 'كبيرة', data: [1, 2, 2, 3] },
       { type: 'متوسطة', data: [1, 4, 1, 2] },
       { type: 'بسيطة', data: [1, 5, 3, 2] },
-      { type: 'وفيات', data: [0, 1, 2, 2] },
-      { type: 'محبطة', data: [0, 0, 3, 2] },
-      { type: 'غير محبطة', data: [0, 0, 2, 3] },
-      { type: 'انهاء البلاغ', data: [0, 4, 2, 2] },
     ];
 
     this.chartOptions = {
@@ -83,6 +78,9 @@ export class StatisticComponent implements OnInit {
 
   ngOnInit(): void {
     this.updateCityNames();
+    // const cities = chartData[2023].quarter1.easternSector;
+
+    // console.log(cities);
   }
   updateCityNames(): void {
     this.cityNames = this.cities[this.selectedSector] || [];
